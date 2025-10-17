6071f15708bc7e55866828c83a22cb8b8f888224
RuinedVLR <rzhurakhov@gmail.com>
2025-10-17 16:38:13 -0300

    50% done

--- VirusSimulator_Roman/Program.cs
+++ VirusSimulator_Roman/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace VirusSimulator_Roman
@@ -29,11 +30,12 @@ namespace VirusSimulator_Roman
 
         static List<(int, int)> virusPosition = new List<(int, int)>();
         static Random rand = new Random();
+        static int virusCount = 0;
 
         static void Main(string[] args)
         {            
             DrawLandscape();
-            VirusSpawn();
+            VirusMovement();
 
 
         }
@@ -68,28 +70,63 @@ namespace VirusSimulator_Roman
             //Console.WriteLine("-------------------------------");
         }
 
-        static void VirusSpawn()
+        static void VirusMovement()
         {
-            for (int i = 0; i < 3; i++)
+            while (true)
             {
-                int x = rand.Next(0, 30);
-                int y = rand.Next(0, 16);
-                Console.WriteLine(x);
-                Console.WriteLine(y);
+                
 
-                virusPosition.Add((x, y));
-                if (landscape[y,x].Equals('-'))
+                while (virusCount < 3)
                 {
-                    landscape[y, x] = 'X';
-                    Console.Clear();
-                    DrawLandscape();
-                    
+                    int x = rand.Next(0, 30);
+                    int y = rand.Next(0, 16);                    
+                    Console.WriteLine(x);
+                    Console.WriteLine(y);
+
+                    virusPosition.Add((x, y));
+                    if (landscape[y, x].Equals('-'))
+                    {
+                        landscape[y, x] = 'X';
+                        virusPosition.Add((y, x));
+                        Console.Clear();
+                        DrawLandscape();
+                        virusCount++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
-                else
+
+                for (int i = 0; i < virusPosition.Count; i++)
                 {
-                    continue;
+                    int moveNum = rand.Next(1, 5);
+                    Thread.Sleep(1000);
+                    if (moveNum == 1 && virusPosition[i].Item1 > 0 && virusPosition[i].Item1 < 16) // down
+                    {
+                        virusPosition[i] = (virusPosition[i].Item1 + 1, virusPosition[i].Item2);
+                    }
+                    else if (moveNum == 2 && virusPosition[i].Item1 > 0 && virusPosition[i].Item1 < 16) // up
+                    {
+                        virusPosition[i] = (virusPosition[i].Item1 - 1, virusPosition[i].Item2);
+                    }
+                    else if (moveNum == 3 && virusPosition[i].Item2 > 0 && virusPosition[i].Item2 < 30)
+                    {
+                        virusPosition[i] = (virusPosition[i].Item1, virusPosition[i].Item2 + 1);
+                    }
+                    else if (moveNum == 4 && virusPosition[i].Item2 > 0 && virusPosition[i].Item2 < 30)
+                    {
+                        virusPosition[i] = (virusPosition[i].Item1, virusPosition[i].Item2 - 1);
+                    }
                 }
+             
             }
+
+        }
+
+        static void Move()
+        {
+            
         }
 
 
